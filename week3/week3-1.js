754136bbaaee63545fe5e408c0e1f752703f4179
hj3126 <hanyujia19@yahoo.com>
2026-09-17 07:27:19 +0800

    week3-2

--- week3/week3-1.js
+++ week3/week3-1.js
@@ -1,31 +1,16 @@
+let x = 0
+let y = 0
+
 function setup() {
     createCanvas(windowWidth, windowHeight)
-    rectMode(CENTER);
 }
 
 function draw() {
-    background(230)
+    background(220)
 
+    x = lerp(x, mouseX, 0.05)
+    y = lerp(y, mouseY, 0.05) lerp(start, stop, amount)
 
-    translate(width / 2, height / 2)
-
-    fill(255, 100, 150);
-    rect(0, 0, 80, 80);
-
-  
-    translate(150, 0)
-
-    fill(120, 180, 255)
-    rect(0, 0, 80, 80);
-
-    
-    translate(0, 150)
-
-    fill(170, 240, 200)
-    rect(0, 0, 80, 80)
-   
-    translate(-300, 0)
-
-    fill(220, 170, 255)
-    rect(0, 0, 80, 80)
+    circle(x, y, 80)
 }
+
---
+++ week3/week3-2.js
@@ -0,0 +1,83 @@
+let y = 0
+let r = 0
+
+let lerpedMouseX = 0
+let lerpedMouseY = 0
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+
+    angleMode(DEGREES)
+
+    stroke(255)
+    strokeWeight(2)
+    noFill()
+}
+
+function draw() {
+
+    background(0);
+
+    lerpedMouseX = lerp(lerpedMouseX, mouseX, 0.05)
+    lerpedMouseY = lerp(lerpedMouseY, mouseY, 0.05)
+
+    r = lerpedMouseX
+
+
+
+    push();
+
+    translate(3 * width / 4, height / 2)
+    rotate(r)
+    scale(0.5)
+
+    line(0, 0, 200, 0);
+    triangle(-40, 35, 0, -50, 40, 35)
+
+    translate(200, 0)
+    rotate(r)
+    scale(1.5)
+
+    triangle(-40, 35, 0, -50, 40, 35)
+    line(0, 0, 200, 0)
+
+    translate(200, 0)
+    rotate(r)
+
+    triangle(-40, 35, 0, -50, 40, 35)
+    line(0, 0, 200, 0)
+
+    translate(200, 0)
+    rotate(r)
+
+    triangle(-40, 35, 0, -50, 40, 35)
+    pop()
+
+    push()
+
+    translate(width / 4, height / 2)
+    rotate(-r)
+    scale(0.5)
+
+    line(0, 0, 200, 0)
+    triangle(-40, 35, 0, -50, 40, 35)
+
+    translate(200, 0)
+    rotate(r)
+    scale(1.5)
+
+    triangle(-40, 35, 0, -50, 40, 35)
+    line(0, 0, 200, 0)
+    translate(200, 0)
+    rotate(r)
+
+    triangle(-40, 35, 0, -50, 40, 35)
+    line(0, 0, 200, 0)
+
+    translate(200, 0)
+    rotate(r)
+
+    triangle(-40, 35, 0, -50, 40, 35)
+
+    pop()
+}
