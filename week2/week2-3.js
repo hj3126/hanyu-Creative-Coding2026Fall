25b4317059779406a30671ed92ec97cf969a2d9f
hj3126 <hanyujia19@yahoo.com>
2026-09-17 06:33:02 +0800

    change5

--- week2/week2-3.js
+++ week2/week2-3.js
@@ -1,27 +1,41 @@
-let x=100
-let y=100
+let y
+let ySpeed = -5
 
-let xspeed=3
-let yspeed=5
-function setup(){
+function setup() {
     createCanvas(windowWidth, windowHeight)
-rectMode(CENTER)
-}
-function draw(){
-    background(255,150,0  )
-fill(255)
-    rect(x,y,80,80)
-    x = xspeed+x
-    y = yspeed+y
 
-if (x>width || x<40){
-    xspeed = -xspeed
+    y = height - 50
 }
-if (y>height || y<40){
-    yspeed = -yspeed
 
+function draw() {
+
+    background(100, 180, 230)
+
+  fill(255, 220, 0)
+    noStroke()
+
+    circle(width / 2, y, 100)
+
+    y = y + ySpeed
+ if (y < 100) {
+        ySpeed = -ySpeed
+    }
+
+ if (y > height - 50) {
+        ySpeed = -ySpeed
+    }
+
+  fill(70, 110, 80)
+
+    triangle(
+        0, height,
+        width / 4, height / 2,
+        width / 2, height
+    )
+
+    triangle(
+        width / 3, height,
+        width * 2 / 3, height / 2,
+        width, height
+    )
 }
-if(y<40){
-    yspeed = -yspeed
-}
-}   
